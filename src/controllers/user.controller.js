72de11ac7d81c0e61df8a35a841ac7db8d3624a1
GOMEZS25 <gomezse97@gmail.com>
2026-03-06 00:05:05 -0500

    feat: agregar función obtenerUsuario

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -61,4 +61,38 @@ const listarUsuarios = async (req, res) => {
     }
 }
 
-module.exports = { crearUsuario, listarUsuarios };
+
+const obtenerUsuario = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userId = BigInt(id);
+        const users = await prisma.user.findUnique({
+            select: {
+                user_id: true,
+                full_name: true,
+                email: true,
+                role_id: true,
+                is_active: true,
+                created_at: true
+            }, where: { user_id: userId }
+
+        });
+
+        if (!users) {
+            return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+        }
+
+
+        const usersSerializados = {
+            ...users,
+            user_id: users.user_id.toString()
+        };
+        res.json(usersSerializados);
+
+    } catch (error) {
+        console.error('Error al obtener usuario:', error.message);
+        res.status(500).json({ mensaje: 'Error interno del servidor', detalle: error.message });
+    }
+}
+
+module.exports = { crearUsuario, listarUsuarios, obtenerUsuario };
