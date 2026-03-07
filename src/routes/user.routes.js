9482390f078806e06a94a49ec461219c5d885a58
GOMEZS25 <gomezse97@gmail.com>
2026-03-06 22:56:12 -0500

    feat: agregar eliminarUsuario

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { crearUsuario, listarUsuarios, obtenerUsuario, actualizarUsuario } = require('../controllers/user.controller');
+const { crearUsuario, listarUsuarios, obtenerUsuario, actualizarUsuario, eliminarUsuario } = require('../controllers/user.controller');
 
 const { verificarToken } = require('../middlewares/auth.middleware');
 
@@ -17,5 +17,9 @@ router.get("/:id", verificarToken, obtenerUsuario);
 
 
 router.put("/:id", verificarToken, actualizarUsuario);
+
+//Eliminar usuario
+router.delete("/:id", verificarToken, eliminarUsuario);
+
 // Exportar router
 module.exports = router;
