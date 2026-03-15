f496205663364bf33c92325c81248f2ba73dfb4e
GOMEZS25 <gomezse97@gmail.com>
2026-03-14 19:20:42 -0500

    Crear-locations

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
 const categoryRoutes = require('./routes/category.routes');
 const productRoutes = require('./routes/product.routes');
+const inventoryRoutes = require('./routes/inventory.routes');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/inventory', inventoryRoutes);
 
 // Ruta de prueba
 app.get('/', (req, res) => {
