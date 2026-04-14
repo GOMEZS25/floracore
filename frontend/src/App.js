730538d45365be8c49f6814220b1ea9a9dcc243e
GOMEZS25 <gomezse97@gmail.com>
2026-04-13 19:55:09 -0500

    feat: add app layout with sidebar navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
+import AppLayout from './components/Layout/AppLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Ruta pública */}
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+
+        {/* Rutas protegidas envueltas en AppLayout */}
+        <Route
+          path="/home"
+          element={
+            <AppLayout>
+              <Home />
+            </AppLayout>
+          }
+        />
+
+        {/* Agrega aquí el resto de rutas protegidas con el mismo patrón:
+            <Route path="/inventory/lots" element={<AppLayout><Lots /></AppLayout>} /> */}
       </Routes>
     </BrowserRouter>
   );
